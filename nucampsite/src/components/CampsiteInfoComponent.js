4abb5d73e2f0a87f7e8651e91c5b17e31480e4c8
Wayne <wayne.thompsonjr@gmail.com>
2022-03-08 20:39:20 -0600

    completed week 3 exercise 1. Rearranged my code to follow a pattern of using presentational and container components

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-
 class CampsiteInfo extends Component {
     constructor(props) {
         super(props);
@@ -18,7 +17,6 @@ class CampsiteInfo extends Component {
                         <CardText>{campsite.description}</CardText>
                     </CardBody>
                 </Card>
-
             </div>
         );
     }
@@ -40,9 +38,11 @@ class CampsiteInfo extends Component {
     render() {
         if(this.props.campsite) {
             return (
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             );
         } 
