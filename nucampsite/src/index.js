c3faae0b057d3c9207201fc87c4343c518d8343f
Wayne <wayne.thompsonjr@gmail.com>
2022-03-10 21:06:14 -0600

    Added bootstrap social

--- nucampsite/src/index.js
+++ nucampsite/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
