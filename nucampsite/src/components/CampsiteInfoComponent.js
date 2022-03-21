eee32f936f0a3223f9cd54b6885238d748b6a7f7
Wayne Thompson <wayne.thompsonjr@gmail.com>
2022-03-21 07:47:27 -0500

    added error handling to author field in comment modal

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -4,6 +4,10 @@ import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, 
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+
 class CommentForm extends Component {
     constructor(props) {
         super(props);
@@ -61,7 +65,25 @@ class CommentForm extends Component {
                                         id="author" 
                                         name="author" 
                                         className="form-control" 
+                                        validators={{
+                                            required, 
+                                            minLength: minLength(2),
+                                            maxLength: maxLength(15)
+                                        }}
                                     />
+                                    
+                                    <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be at least 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                    />
+
                             </div>
 
                             <div className="form-group">
