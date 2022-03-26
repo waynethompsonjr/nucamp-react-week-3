9077032b1af751a4c93cc87a54f8c82f65aa8634
Wayne Thompson <wayne.thompsonjr@gmail.com>
2022-03-25 20:13:46 -0500

    redux thunk

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -3,6 +3,8 @@ import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, 
     Form, FormGroup, Input, Label, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
+
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -149,8 +151,28 @@ function RenderComments({comments, addComment, campsiteId}) {
         return (<div />);
     }
 
-function CampsiteInfo(props) {
-    if(props.campsite) {
+    function CampsiteInfo(props) {
+        if (props.isLoading) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        if (props.errMess) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+                            <h4>{props.errMess}</h4>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        if (props.campsite) {
         return (
             <div className="container">
                 <div className="row">
